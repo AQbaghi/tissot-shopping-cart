a995a526faeab984becd084f2673aed421d433bb
unknown <abdalqaderbaghi@gmail.com>
2020-09-10 22:14:34 +0100

    completed the ejs rendering process from json file

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv');
+}
+const fs = require('fs');
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
+
 const express = require('express');
 const app = express();
 
+//EnterStripeMan098ChaChing
+
+app.get('/store', (req, res) => {
+  fs.readFile('items.json', (err, data) => {
+    if (err) {
+      res.status(500).end();
+    } else {
+      res.render('store.ejs', {
+        items: JSON.parse(data),
+      });
+    }
+  });
+});
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.listen(3000);
+app.listen(5000);
